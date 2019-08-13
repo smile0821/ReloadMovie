4ccc494b2eee88368c103e243033ced68ac82f55
simel0821 <Wuyao821@126.com>
2019-08-13 23:26:59 +0800

    数据绑定与模板

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,16 @@
 Page({
-  
+  onTap: function(){
+    // wx.navigateTo({
+    //   url: '../posts/post'
+    // })
+    wx.redirectTo({
+      url: '../posts/post'
+    })
+  },
+  onContainerTap: function(){
+    console.log('Container tap')
+  },
+  onSubTap: function(){
+    console.log('Sub tap')
+  }
 })
